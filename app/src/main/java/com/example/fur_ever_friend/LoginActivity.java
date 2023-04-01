a0f797051630f9f316d10baf67dc4ba89e166f0c
Ronish29 <ronishparmar2003@gmail.com>
2023-04-01 18:45:59 +0530

    Login Activity Added and Register activity updated

---
+++ app/src/main/java/com/example/fur_ever_friend/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.fur_ever_friend;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button newactivity=findViewById(R.id.btn_jenish);
+        newactivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LoginActivity.this,Register.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
