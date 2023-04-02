e512b5636df5fc35bbd03aaf9cbf63861d75b31a
Jenis07 <jenishjadav17@gmail.com>
2023-04-02 10:53:33 +0530

    Show Hide Password Added

--- app/src/main/java/com/example/fur_ever_friend/LoginActivity.java
+++ app/src/main/java/com/example/fur_ever_friend/LoginActivity.java
@@ -3,15 +3,23 @@ package com.example.fur_ever_friend;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
     Button register,forget;
+    EditText password;
+    ImageView showHide;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        password=findViewById(R.id.full_name);
+        showHide=findViewById(R.id.showhide);
         Button register=findViewById(R.id.register_btn);
         Button forget=findViewById(R.id.forget_pass);
         register.setOnClickListener(new View.OnClickListener() {
@@ -30,5 +38,14 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+    }
+    public void ShowHide(View view) {
+        if(password.getTransformationMethod().equals(PasswordTransformationMethod.getInstance())){
+            password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            showHide.setImageResource(R.drawable.lock_item);
+        }else{
+            password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            showHide.setImageResource(R.drawable.baseline_remove_red_eye_24);
+        }
     }
 }
